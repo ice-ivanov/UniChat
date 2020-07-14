b5dd6f5acb4478ead320f730f219da66ac154e6f
Ice Ivanov <ice.ivanov@gmail.com>
2020-07-14 08:51:27 +0300

    testing 2

--- UniChat/routing.py
+++ UniChat/routing.py
@@ -1,8 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.contrib import admin
-from django.urls import path
 
 application = ProtocolTypeRouter({
     'websocket': AllowedHostsOriginValidator(
